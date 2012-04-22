0f7795996ff1a0749ee9f35646e19e0293300369
Chris Andrews <chris@nodnol.org>
2012-04-22 21:59:18 +0100

    Reload model YAMLs every time.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :reload_yaml
   before_filter :load_ranges
 
   private
+
+  def reload_yaml
+    [ Product, Collection, ProductRange ].each do |model|
+      model.reload(true)
+    end
+  end
 
   def load_ranges
     @collections = Collection.find(:all)

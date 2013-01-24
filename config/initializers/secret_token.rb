8fa467b5ba1d05a9798a96e5744060d19618ca15
Chris Andrews <chris@nodnol.org>
2013-01-24 21:44:12 +0000

    Remove session and cookie token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Brightonbelles::Application.config.secret_token = '475fc62bde136f17b53dbd13e367bc6fdd43903dd3a49906e2059f9c3634559e56c1ddc6edf085deef2e12e0160076c34998bfc41a6e398cb499b38f806ac8e3'
+Brightonbelles::Application.config.secret_token = ''
